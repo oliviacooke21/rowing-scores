fcbf1a0150374f7ec45229c2566f95e6cdbf639c
oliviacooke21 <ocooke@fas.harvard.edu>
2026-05-18 14:42:31 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -60,7 +60,14 @@ def scrape_conference(conf_key):
     race_results_found = {}
 
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+    "Referer": "https://results.regattatiming.com/",
+    "Upgrade-Insecure-Requests": "1",
+}
     }
 
     # ---- FETCH PAGE ----
